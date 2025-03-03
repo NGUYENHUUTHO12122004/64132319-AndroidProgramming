69273dd63e576e2e7db657a05626fcb4b92f9dcc
NGUYENHUUTHO12122004 <tho.nh.64cntt@ntu.edu.vn>
2025-03-03 11:13:22 +0700

    update AppCong

--- app/src/main/java/till/edu/appcong/MainActivity.java
+++ app/src/main/java/till/edu/appcong/MainActivity.java
@@ -1,6 +1,8 @@
 package till.edu.appcong;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import till.edu.helloandroid.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        };
+    public void XuLyCong(View view){
+        EditText editTextSoA = findViewById(R.id.edtA);
+        EditText editTextSoB = findViewById(R.id.edtB);
+        EditText editTextKetQua = findViewById(R.id.edtKQ);
+
+        String strA = editTextSoA.getText().toString();
+        String strB = editTextSoA.getText().toString();
+
+        int so_A = Integer.parseInt(strA);
+        int so_B = Integer.parseInt(strB);
+
+        int  tong = so_A + so_B;
+        String strTong = String.valueOf(tong);
+        editTextKetQua.setText(strTong);
     }
 }
